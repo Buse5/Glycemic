6ca04ba72c87981bc4c2df6356b71684e69d99e7
BuseYalcin <buseylcin5@gmail.com>
2022-02-08 18:38:01 +0300

    Initial Commit

--- src/main/java/com/works/glycemic/config/SecurityConfig.java
+++ src/main/java/com/works/glycemic/config/SecurityConfig.java
@@ -25,6 +25,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .cors().and()
                 .httpBasic()
                 .and()
                 .authorizeHttpRequests()
